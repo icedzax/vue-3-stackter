b80332afa6215ddf472169e19438ca0b1bd16752
icedzax <your_email_address@icedza@gmail.com>
2022-06-02 08:12:35 +0700

    api steam

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -26,14 +26,8 @@
     </section> -->
     <section>
         <div class="m-3 flex justify-center space-x-0.5 w-full">
-            <div v-for="h in storeHeroes.slice(0, 10)" :key="h.id">
-                <img
-                    class="rounded-sm"
-                    :src="`http://cdn.dota2.com/apps/dota2/images/heroes/${h.name
-                        .replace(' ', '_')
-                        .toLowerCase()}_sb.png`"
-                    alt=""
-                />
+            <div v-for="heroes in storeHeroes.slice(0, 20)" :key="heroes.id">
+                <span>{{ heroes.name }}</span>
             </div>
         </div>
     </section>
@@ -44,7 +38,7 @@ import { useMeta } from 'vue-meta'
 import { computed, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
-import { LiquipediaApi } from 'liquipedia-api'
+import axios from 'axios'
 
 useMeta({
     title: 'Homepage',
@@ -67,10 +61,6 @@ function saveName() {
     router.push(`/about/${name.value}`)
 }
 
-const liquipediaApi = new LiquipediaApi({
-    USER_AGENT: 'MyAwesomeProject/1.0 (my.email@gmail.com)',
-})
-
 const heroes = ref([])
 const storeHeroes = computed(() => store.getters['dota/getHeroes'])
 
@@ -80,8 +70,15 @@ async function fetchHeroes() {
         return
     }
     console.log('FEATCHING HEROES')
-    const fHeroes = await liquipediaApi.dota.getHeroes()
-    heroes.value = fHeroes
+    try {
+        const data = await axios.get(
+            'https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v1?key=D92ADA35EC43FAF861D0200FEFCAA05B'
+        )
+        heroes.value = data.data
+    } catch (error) {
+        console.log(error)
+    }
+
     store.dispatch('dota/initHeroes', heroes.value)
 }
 </script>
