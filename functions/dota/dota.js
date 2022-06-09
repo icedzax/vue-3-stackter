c5c5f40012802227fe6dfad94660ed67a1bcf04f
icedzax <your_email_address@icedza@gmail.com>
2022-06-09 10:23:01 +0700

    hh

--- functions/dota/dota.js
+++ functions/dota/dota.js
@@ -1,32 +1,24 @@
 const fetch = require('node-fetch')
 
-const handler = async function () {
-    try {
-        const response = await fetch(
-            'https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v1?key=D92ADA35EC43FAF861D0200FEFCAA05B',
-            {
-                headers: { Accept: 'application/json' },
-            }
-        )
-        if (!response.ok) {
-            // NOT res.status >= 200 && res.status < 300
-            return { statusCode: response.status, body: response.statusText }
-        }
-        const data = await response.json()
+/* Get IP address https://www.ipify.org/ */
+const API_ENDPOINT =
+    'https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v1?key=D92ADA35EC43FAF861D0200FEFCAA05B'
 
+exports.handler = async (event, context) => {
+    try {
+        const data = await fetch(API_ENDPOINT, {
+            method: 'GET',
+        }).then((response) => response.json())
         return {
             statusCode: 200,
-            body: JSON.stringify({ msg: data.joke }),
+            body: JSON.stringify(data),
         }
-    } catch (error) {
-        // output to netlify function log
-        console.log(error)
+    } catch (e) {
         return {
-            statusCode: 500,
-            // Could be a custom message or object i.e. JSON.stringify(err)
-            body: JSON.stringify({ msg: error.message }),
+            statusCode: 422,
+            body: JSON.stringify({
+                e: e.message,
+            }),
         }
     }
 }
-
-module.exports = { handler }
